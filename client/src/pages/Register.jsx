0ef5887f3b80abd5a93ddfdcd343868e78ca3ea1
YPhuong15 <yenphuongtong97@gmail.com>
2024-03-15 20:55:10 -0700

    update register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,22 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
     return (
         <Form>
-            <Row className="mb-3">
-                <Form.Group as={Col} md='6'>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control required type='text' placeholder="Frist name" />
-                </Form.Group>
-                <Form.Group as={Col} md='6'>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control required type='text' placeholder="Last name" />
-                </Form.Group>
-            </Row>
+            <Form.Group as={Col} md='6'>
+                <Form.Label> Name</Form.Label>
+                <Form.Control required type='text' placeholder="name" />
+            </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
@@ -29,9 +22,9 @@ export default function Register() {
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
             <Link to='/profile'>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Link>
         </Form>
     );
