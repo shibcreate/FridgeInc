af4a5f654c2ba148ce3468f3b1842a02750eac02
YPhuong15 <yenphuongtong97@gmail.com>
2024-03-29 01:56:21 -0700

    delete unused attribute

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ObjectDetector } from "./objectD/objectDetector";
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 function Profile() {
   const [image, setImage] = useState(null);
