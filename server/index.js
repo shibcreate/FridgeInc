0301fa63fb18c4334267795facf0935effa9f709
YPhuong15 <yenphuongtong97@gmail.com>
2024-03-28 21:12:16 -0700

    JWT need to install dotenv bcrypt jsonwebtoken

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const mongoose = require('mongoose');
 const cors = require("cors");
 const UserModel = require("./models/Users");
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser')
+const {createToken, validateToken} = require('./middleware/auth')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // MongoDB connection
 mongoose.connect("mongodb+srv://admin:Testing1234!@fridgeinc.rufgqkq.mongodb.net/?retryWrites=true&w=majority&appName=fridgeinc", {
@@ -21,34 +25,50 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-app.post('/login', (req, res) => {
+app.post('/register', (req, res) => {
   const { email, password } = req.body;
-  UserModel.findOne({ email: email })
-    .then(user => {
-      if (user) {
-        if (user.password === password) {
-          res.json("Success");
-        } else {
-          res.json("The password is incorrect");
-        }
-      } else {
-        res.json("No record existed");
+  bcrypt.hash(password, 10).then((hash) => {
+    UserModel.create({
+      email: email,
+      password: hash
+    }). then(() => {
+      res.json("User registered");
+    }).catch((err) => {
+      if (err) {
+        res.status(400).json({error: err});
       }
-    })
-    .catch(err => {
-      console.error('Error during login:', err);
-      res.status(500).json({ error: 'Internal server error' });
     });
+  });
 });
 
-app.post('/register', (req, res) => {
-  UserModel.create(req.body)
-    .then(employees => res.json(employees))
-    .catch(err => {
-      console.error('Error during registration:', err);
-      res.status(500).json({ error: 'Internal server error' });
-    });
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email: email });
+      if (!user) 
+      res.status(401).json({error: "User is not registered"});
+      const hashedPassword = user.password;
+      bcrypt.compare(password, hashedPassword).then((match) => {
+        if (!match) {
+          res.status(400).json({error: "Wrong password"});
+        } else {
+          const accessToken = createToken(user);
+          res.cookie('access-token', accessToken, {
+            maxAge: 1296000000,
+            httpOnly: true,
+          });
+          res.json('Logged in');
+          console.log('logged in')
+        }
+      });
 });
+
+app.get('/profile', validateToken, (req, res) => {
+  res.json('profile page')
+});
+
+app.get('/recipes', validateToken, (req, res) => {
+  res.json('recipe list')
+})
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
---
+++ server/middleware/auth.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const {sign, verify} = require('jsonwebtoken')
+
+const createToken = (user) => {
+    const accessToken = sign({email: user.email}, process.env.JWT_SECRET);
+    return accessToken;    
+    
+};
+
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies['access-token'];
+    if (!accessToken) 
+    return res.status(401).json({error: "User not authenticated"});
+    try {
+        const validToken = verify(accessToken, process.env.JWT_SECRET)
+        if (validToken) {
+            req.authenticated = true;
+            return next();
+        }
+    } catch(err) {
+        return res.status(400).json({error: err});
+    }
+};
+module.exports = {createToken, validateToken};
