e1082674edba9c2568e9acd80295b6036e6b3b4e
ritsukye <131490708+ritsukye@users.noreply.github.com>
2024-04-02 15:47:54 -0700

    finished bio and pfp color
    stored in local storage p sure it works!!!

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
 export default function Navigation() {
+
   return (
       <Navbar bg="light" data-bs-theme="light">
         <Container fluid>
@@ -21,7 +22,18 @@ export default function Navigation() {
           </Navbar.Brand>
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/recipes">Recipes</Nav.Link>
-            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="/profile">Profile
+            <div
+              style={{
+                width: '20px',
+                height: '20px',
+                borderRadius: '50%',
+                backgroundColor: localStorage.getItem('userColor'),
+                display: 'inline-block',
+                marginLeft: '5px',
+              }}
+            ></div>
+            </Nav.Link>      
           </Nav>
           <Link to='/login'>
           <Button variant="outline-success">Log In</Button>
