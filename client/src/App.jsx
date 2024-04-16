20eeeccaa542a1e879dcdfecf41ee612d24f504c
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2024-04-16 13:25:16 -0700

    working resetting of localStorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { useState } from 'react';
-//import pages
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import pages
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import Login from './pages/Login';
@@ -11,35 +11,42 @@ import Upload from './pages/Upload';
 import RecipesList from './pages/RecipesList';
 import Profile from './pages/Profile';
 import NewPost from './pages/NewPost';
-//import components
-import Navigation from './components/Navigation'
-import Footer from './components/Footer'
+// import components
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    const hasClearedLocalStorage = localStorage.getItem('hasClearedLocalStorage');
+    if (!hasClearedLocalStorage) {
+      localStorage.clear();
+      localStorage.setItem('hasClearedLocalStorage', 'true');
+    }
+  }, []);
+
   const handleLogout = () => {
     setIsLoggedIn(false);
-    setKey(prevKey => prevKey + 1);
   };
 
   return (
     <div className="App">
       <BrowserRouter>
-      <Navigation key={key} isLoggedIn={isLoggedIn} handleLogout={handleLogout}/>
+        <Navigation isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
         <main>
-        <Routes>
-          <Route path="/" element={<Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-          <Route path="/about" element={<About/>} />
-          <Route path="/login" element={<Login setLoggedIn={setIsLoggedIn}/>} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
-          <Route path="/upload" element={<Upload isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} /> //protected route
-          <Route path="/recipes" element={<RecipesList isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} /> //protected route
-          <Route path="/share-recipe" element={<NewPost isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} /> //protected route
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login setLoggedIn={setIsLoggedIn} />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+            <Route path="/upload" element={<Upload isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} /> {/* protected route */}
+            <Route path="/recipes" element={<RecipesList isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} /> {/* protected route */}
+            <Route path="/share-recipe" element={<NewPost isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} /> {/* protected route */}
+          </Routes>
         </main>
-      <Footer />
+        <Footer />
       </BrowserRouter>
     </div>
   );
